a01287afb424940ac58efafd0c2003a3b362cadd
Zhao Zhili <quinkblack@foxmail.com>
2021-10-13 18:02:56 +0800

    demux: mp4: add an option to ignore editlist
    refs #22391
    
    (cherry picked from commit b9e50b090cf97fcb595dee4bfcec6e809818c51d)

--- modules/demux/mp4/mp4.c
+++ modules/demux/mp4/mp4.c
@@ -50,6 +50,8 @@ static void Close( vlc_object_t * );
 #define MP4_M4A_TEXT     N_("M4A audio only")
 #define MP4_M4A_LONGTEXT N_("Ignore non audio tracks from iTunes audio files")
 
+#define MP4_ELST_TEXT       N_("Handle edit list")
+
 vlc_module_begin ()
     set_category( CAT_INPUT )
     set_subcategory( SUBCAT_INPUT_DEMUX )
@@ -60,6 +62,7 @@ vlc_module_begin ()
 
     add_category_hint("Hacks", NULL, true)
     add_bool( CFG_PREFIX"m4a-audioonly", false, MP4_M4A_TEXT, MP4_M4A_LONGTEXT, true )
+    add_bool( CFG_PREFIX"editlist", true, MP4_ELST_TEXT, MP4_ELST_TEXT, true )
 vlc_module_end ()
 
 /*****************************************************************************
@@ -3456,7 +3459,8 @@ static void MP4_TrackSetup( demux_t *p_demux, mp4_track_t *p_track,
     const MP4_Box_t *p_elst;
     p_track->i_elst = 0;
     p_track->i_elst_time = 0;
-    if( ( p_track->p_elst = p_elst = MP4_BoxGet( p_box_trak, "edts/elst" ) ) )
+    p_track->p_elst = NULL;
+    if( ( p_elst = MP4_BoxGet( p_box_trak, "edts/elst" ) ) )
     {
         MP4_Box_data_elst_t *elst = BOXDATA(p_elst);
         unsigned int i;
@@ -3473,8 +3477,12 @@ static void MP4_TrackSetup( demux_t *p_demux, mp4_track_t *p_track,
                      elst->i_media_rate_integer[i],
                      elst->i_media_rate_fraction[i] );
         }
-    }
 
+        if( var_InheritBool( p_demux, CFG_PREFIX"editlist" ) )
+            p_track->p_elst = p_elst;
+        else
+            msg_Dbg( p_demux, "ignore editlist" );
+    }
 
 /*  TODO
     add support for:
