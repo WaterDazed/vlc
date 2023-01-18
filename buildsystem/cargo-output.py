6fc4d3c8369fe5d8cc53abadb6bda041fa62651f
Loïc <loic@videolabs.io>
2023-01-18 10:45:57 +0100

    vlcrs: add meson infra for building and linking Rust modules
    Despite meson officially "supporting" Rust it does support working with
    Cargo (Rust package manager and build system) so we need to have
    a bit of custom system for using it. `cargo-output.py` to build and copy
    the output files (static archive and depfile) to the expected location
    by meson. And `cargo-rustc-static-libs.py` to get the common linker args
    for a bar-bone std rust program (ie without dependencies).
    
    Those two scripts would potentially be removed if meson and/or cargo
    became more flexible and/or intelligent.

--- buildsystem/cargo-output.py
+++ buildsystem/cargo-output.py
@@ -16,7 +16,6 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
 
-
 # This is a wrapper/interceptor script for Cargo (the Rust package manager) that adds
 # the ability to specify an output directory for the final compiler artifact
 # (archive lib, depfile).
