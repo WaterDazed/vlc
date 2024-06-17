98c9fdf39a98a6ec520d10c3b82bdced9b17e448
Vikram Kangotra <vikramkangotra8055@gmail.com>
2024-06-18 00:09:14 +0530

    wasm_extension: Add script scanning

--- modules/wasm-rs/src/extension.rs
+++ modules/wasm-rs/src/extension.rs
@@ -1,36 +1,75 @@
-use vlcrs_core::debug;
+use std::path::Path;
+
+use vlcrs_core::{debug, error};
 use vlcrs_core::error::Result;
 use vlcrs_core::messages::Logger;
-use vlcrs_core::module::extension::Module;
-
-use vlcrs_core::module::extension::ThisExtensionsManager;
+use vlcrs_core::module::extension::{ExtensionManager, ExtensionManagerControl, Module, ThisExtensionsManager};
 use vlcrs_core::module::ModuleArgs;
 
-use vlcrs_core::extension::{ExtensionManager, ExtensionManagerControl, Extension};
+use vlcrs_core::extension::Extension;
 
 use vlcrs_core::input_item::InputItem;
 
+use crate::vlcwasm_scripts_batch_execute;
+use crate::ProvidesLogger;
+
 pub struct WasmExtensionModule;
 
-pub struct WasmExtensionManager;
+struct WasmExtensionManager<'a> {
+    logger: &'a mut Logger,
+}
+
+impl ProvidesLogger for WasmExtensionManager<'_> {
+    fn logger(&mut self) -> &mut Logger {
+        self.logger
+    }
+}
 
 impl Module for WasmExtensionModule {
     fn open<'a> (
         _this_extension_manager: ThisExtensionsManager,
         logger: &'a mut Logger,
         _args: &mut ModuleArgs,
-    ) -> Result<Box<dyn ExtensionManager>> {
+    ) -> Result<Box<dyn ExtensionManager + 'a>> {
 
         debug!(logger, "Wasm extensions manager module loaded");
 
-        Ok(Box::new(WasmExtensionManager))
+        let mut wasm_extension_manager = WasmExtensionManager {
+            logger,
+        };
+
+        wasm_extension_manager.scan_extensions()?;
+
+        Ok(Box::new(wasm_extension_manager))
     }
 }
 
-impl ExtensionManager for WasmExtensionManager {
+impl<'a> WasmExtensionManager<'a> {
+
+    fn scan_extensions(&mut self) -> Result<()> {
+        let dir_name = Path::new("extensions");
+        if let Err(e) = vlcwasm_scripts_batch_execute(self, dir_name, Self::scan_callback) {
+            error!(self.logger, "Can't load extension module");
+            return Err(e);
+        }
+        Ok(())
+    }
+
+    fn scan_callback(&mut self, file_name: &Path) -> Result<()> {
+        debug!(self.logger, "Scanning Wasm script {}", file_name.display());
+
+        let mut extension = Extension::new();
+        extension.set_name(file_name.to_str().expect("Should be a valid Utf-8"))?;
+        extension.set_logger(self.logger)?;
+        
+        Ok(())
+    }
 }
 
-impl ExtensionManagerControl for WasmExtensionManager {
+impl<'a> ExtensionManager for WasmExtensionManager<'a> {
+}
+
+impl<'a> ExtensionManagerControl for WasmExtensionManager<'a> {
     fn activate(&self, _extension: Extension) -> Result<()> {
         unimplemented!()
     }
--- modules/wasm-rs/src/lib.rs
+++ modules/wasm-rs/src/lib.rs
@@ -1,10 +1,84 @@
 mod extension;
 mod intf;
 
+use std::fs;
+use std::path::{Path, PathBuf};
+
+use vlcrs_core::{error::Result, messages::Logger};
 use vlcrs_core_macros::module;
+
+use vlcrs_core::{debug, error};
+
+use vlcrs_core::configuration::{get_user_dir, get_sys_path, UserDir, SysDir};
 
 use crate::intf::Wasm;
 use crate::extension::WasmExtensionModule;
+
+type ScanCallbackFn<T> = fn(&mut T, &Path) -> Result<()>;
+
+fn vlcwasm_dir_list(dir_name: &Path) -> Result<Vec<PathBuf>> {
+
+    let append_wasm_dir = |base_dir: &Path| -> PathBuf {
+        base_dir.join("wasm").join(dir_name)
+    };
+
+    let mut paths = Vec::with_capacity(3);
+
+    // Add user directory path if available
+    if let Ok(user_dir) = get_user_dir(UserDir::VLC_USERDATA_DIR) {
+        paths.push(append_wasm_dir(&user_dir));
+    }
+
+    let lib_dir = get_sys_path(SysDir::VLC_PKG_LIBEXEC_DIR);
+    let data_dir = get_sys_path(SysDir::VLC_PKG_DATA_DIR);
+
+    if let Ok(data_dir) = &data_dir {
+        // Add data_dir if lib_dir is not available or if lib_dir is a different path
+        if lib_dir.as_ref().map_or(true, |lib_dir| lib_dir != data_dir) {
+            paths.push(append_wasm_dir(data_dir));
+        }
+    }
+
+    // Add library directory path if available
+    if let Ok(lib_dir) = lib_dir {
+        paths.push(append_wasm_dir(&lib_dir));
+    }
+
+    Ok(paths)
+}
+
+fn vlcwasm_scripts_batch_execute<T: ProvidesLogger>(p_this: &mut T,dir_name: &Path, func: ScanCallbackFn<T>) -> Result<()> {
+
+    let dir_list = vlcwasm_dir_list(dir_name)?;
+
+    let mut script_loaded = false;
+
+    for dir in dir_list {
+
+        debug!(p_this.logger(), "Trying Wasm scripts in {}", dir.display());
+
+        if let Ok(files) = fs::read_dir(&dir) {
+            for file in files.flatten() {
+                let path = file.path();
+                debug!(p_this.logger(), "Trying Wasm script: {}", path.display());
+
+                if let Ok(_) = func(p_this, &path) {
+                    script_loaded = true
+                }
+            }
+        };   
+    }
+
+    if script_loaded {
+        Ok(())
+    } else {
+        Err(error::CoreError::Unknown)
+    }
+}
+
+pub trait ProvidesLogger {
+    fn logger(&mut self) -> &mut Logger;
+}
 
 module! {
     type: Wasm,
