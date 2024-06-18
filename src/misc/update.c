WaterDazed/vlc

commit c79116dc6119cae6521ffe95acf485ed749660de

update: don't pretend all debug builds are Win32
If you want to test this on something else, then update the corresponding
status file, seriously.

src/misc/update.c
@@ -82,9 +82,9 @@
 #endif
 
 #ifndef NDEBUG
-# define UPDATE_VLC_STATUS_URL "http://update-test.videolan.org/vlc/status-win-x86"
+# define UPDATE_VLC_STATUS_URL "http://update-test.videolan.org/vlc/status"
 #else
-# define UPDATE_VLC_STATUS_URL "http://update.videolan.org/vlc/status" UPDATE_OS_SUFFIX
+# define UPDATE_VLC_STATUS_URL "http://update.videolan.org/vlc/status"
 #endif
 
 #define dialog_FatalWait( p_obj, psz_title, psz_fmt, ... ) \
@@ -179,11 +179,12 @@ static void EmptyRelease( update_t *p_update )
  */
 static bool GetUpdateFile( update_t *p_update )
 {
+    static const char url[] = UPDATE_VLC_STATUS UPDATE_OS_SUFFIX;
     stream_t *p_stream = NULL;
     char *psz_version_line = NULL;
     char *psz_update_data = NULL;
 
-    p_stream = vlc_stream_NewURL( p_update->p_libvlc, UPDATE_VLC_STATUS_URL );
+    p_stream = vlc_stream_NewURL( p_update->p_libvlc, url );
     if( !p_stream )
     {
         msg_Err( p_update->p_libvlc, "Failed to open %s for reading",

commit bfbbb983fa1c756130957b05dc1545f6a43c575c

update: fix URL in signature check
The URL used contains the platform.
Regression from c79116dc6119cae6521ffe95acf485ed749660de.

src/misc/update.c
@@ -281,7 +281,7 @@ static bool GetUpdateFile( update_t *p_update )
      * to authenticate it */
     signature_packet_t sign;
     if( download_signature( VLC_OBJECT( p_update->p_libvlc ), &sign,
-            UPDATE_VLC_STATUS_URL ) != VLC_SUCCESS )
+                            url ) != VLC_SUCCESS )
     {
         msg_Err( p_update->p_libvlc, "Couldn't download signature of status file" );
         goto error;