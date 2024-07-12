bdc7fe43730a52147bc3d6a11a42aa4b9080326c
Khalid Masum <khalid.masum.92@gmail.com>
2024-07-12 15:28:41 +0000

    demux: mkv: Implement timeout and fatal error handler

--- modules/demux/mkv/chapter_command_matroska_js.cpp
+++ modules/demux/mkv/chapter_command_matroska_js.cpp
@@ -10,7 +10,15 @@
 
 #include "duktape/duktape.h"
 
+duk_bool_t duk_check_timeout(void *udata) {
+    if (udata == nullptr) return false;
+    auto data = static_cast<mkv::matroska_js_interpreter_c*>(udata);
+    return data->timed_out;
+}
+
 namespace mkv {
+
+constexpr vlc_tick_t INTERPRETER_TIMEOUT = VLC_TICK_FROM_SEC(3);
 
 //MatroskaJS
 const char* matroska_js_interpreter_c::CMD_MS_GOTO_AND_PLAY = "GotoAndPlay";
@@ -115,11 +123,23 @@ duk_ret_t matroska_js_interpreter_c::js_execute_LogMsg(duk_context *ctx)
     return 0;
 }
 
+void matroska_js_interpreter_c::on_timeout()
+{
+    vlc_error(l,"Script taking too long (%" PRId64 ") to execute, stopping", SEC_FROM_VLC_TICK(INTERPRETER_TIMEOUT));
+    timed_out = true;
+}
+
+void matroska_js_interpreter_c::fatal_error_handler(void *udata, const char* msg)
+{
+    auto metadata_obj = static_cast<matroska_js_interpreter_c*> (udata);
+    vlc_error( metadata_obj->l, "DUKTAPE: FATAL ERROR OCCURED DURING JS EXECUTION:\n %s", msg );
+    throw std::runtime_error("DUKTAPE FATAL");
+}
 
 
 duk_context* matroska_js_interpreter_c::ms_setup()
 {
-    duk_context *ctx = duk_create_heap_default();
+    duk_context *ctx = duk_create_heap(NULL, NULL, NULL, this, fatal_error_handler);
 
     duk_push_c_function(ctx, js_execute_GotoAndPlay, 1);
     duk_put_global_string(ctx, CMD_MS_GOTO_AND_PLAY);
@@ -140,17 +160,41 @@ bool matroska_js_interpreter_c::Interpret( const binary * p_command, size_t i_si
 {
     bool success = false;
     std::string sz_command( reinterpret_cast<const char*> (p_command), i_size );
+    vlc_timer_t timer;
 
     vlc_debug( l, "command input : %s", sz_command.c_str() );
-    success = !duk_peval_lstring(ctx, sz_command.c_str(), sz_command.size());
+    try {
+        if (vlc_timer_create(&timer, [](void *p) {
+            auto p_this = static_cast<matroska_js_interpreter_c *>(p);
+            p_this->on_timeout();
+            }, this))
+        {
+            vlc_debug(l, "Timer initialization failed");
+            return false;
+        }
+        timed_out = false;
+        vlc_timer_schedule(timer, false, INTERPRETER_TIMEOUT, VLC_TIMER_FIRE_ONCE);
+        success = !duk_peval_lstring(ctx, sz_command.c_str(), sz_command.size());
+        vlc_timer_destroy(timer);
+
+        if (timed_out)
+        {
+            vlc_error(l, "Execution TimedOut!\n%s", duk_safe_to_stacktrace(ctx, -1));
+            return false;
+        }
+
+        if (!success)
+        {
+            vlc_error(l, "duktape:: Evaluation Failed!\n%s", duk_safe_to_stacktrace(ctx, -1));
+            return false;
+        }
+    } catch (std::exception e){
+        vlc_error(l, "duktape:: Evaluation Failed!\n%s", duk_safe_to_stacktrace(ctx, -1));
+        return false;
+    }
     vlc_debug(l, "duktape:: Evaluation complete");
 
-    if (!success)
-    {
-        vlc_error(l, "duktape:: Evaluation Failed!\n%s", duk_safe_to_stacktrace(ctx, -1));
-    }
-
-    return success;
+    return true;
 }
 
 } // namespace
