1385da29b7e63d38e1f01de6699dda3d7058ebc2
Francois Cartegnie <fcvlcdev@free.fr>
2024-10-28 19:21:45 +0700

    demux: mp4: map c708 as clcp
    refs #28841
    
    not totally correct, but unless we
    rework everything like 4.0, that's the easy way

--- modules/demux/mp4/libmp4.c
+++ modules/demux/mp4/libmp4.c
@@ -4610,6 +4610,7 @@ static const struct
     /* Subtitles */
     { ATOM_tx3g,    MP4_ReadBox_sample_tx3g,      0 },
     { ATOM_c608,    MP4_ReadBox_sample_clcp,      ATOM_stsd },
+    { ATOM_c708,    MP4_ReadBox_sample_clcp,      ATOM_stsd },
     //{ ATOM_text,    MP4_ReadBox_sample_text,    0 },
     /* In sample WebVTT subtitle atoms. No ATOM_wvtt in normal parsing */
     { ATOM_vttc,    MP4_ReadBoxContainer,         ATOM_wvtt },
