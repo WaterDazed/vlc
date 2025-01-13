62bebc9ed66277e7f35f37aaaa3923e3b81ec797
Steve Lhomme <robux4@ycbcr.xyz>
2025-01-13 15:32:14 +0100

    spudec: fill the region picture early

--- modules/codec/spudec/parse.c
+++ modules/codec/spudec/parse.c
@@ -876,21 +876,16 @@ static int Render( decoder_t *p_dec, subpicture_t *p_spu,
         fmt.p_palette->palette[i_x][3] = p_spu_data->pi_alpha[i_x] * 0x11;
     }
 
-    subpicture_region_t *p_region = subpicture_region_New( &fmt );
+    picture_t *pic = picture_NewFromFormat( &fmt );
     fmt.p_palette = NULL;
     video_format_Clean( &fmt );
-    if( !p_region )
+    if( !pic )
     {
-        msg_Err( p_dec, "cannot allocate SPU region" );
+        msg_Err( p_dec, "cannot allocate SPU picture" );
         return VLC_EGENERIC;
     }
-    vlc_spu_regions_push(&p_spu->regions, p_region);
-
-    p_region->b_absolute = true;
-    p_region->i_x = p_spu_properties->i_x;
-    p_region->i_y = p_spu_properties->i_y + p_spu_data->i_y_top_offset;
-    p_p = p_region->p_picture->p->p_pixels;
-    i_pitch = p_region->p_picture->p->i_pitch;
+    p_p = pic->p->p_pixels;
+    i_pitch = pic->p->i_pitch;
 
     /* Draw until we reach the bottom of the subtitle */
     for( i_y = 0; i_y < height * i_pitch; i_y += i_pitch )
@@ -904,6 +899,21 @@ static int Render( decoder_t *p_dec, subpicture_t *p_spu,
             memset( p_p + i_x + i_y, i_color, i_len );
         }
     }
+
+    subpicture_region_t *p_region = subpicture_region_ForPicture( &pic->format, pic );
+    if( !p_region )
+    {
+        msg_Err( p_dec, "cannot allocate SPU region" );
+        picture_Release(pic);
+        return VLC_EGENERIC;
+    }
+    vlc_spu_regions_push(&p_spu->regions, p_region);
+
+    p_region->b_absolute = true;
+    p_region->i_x = p_spu_properties->i_x;
+    p_region->i_y = p_spu_properties->i_y + p_spu_data->i_y_top_offset;
+    p_p = p_region->p_picture->p->p_pixels;
+    i_pitch = p_region->p_picture->p->i_pitch;
 
     return VLC_SUCCESS;
 }
