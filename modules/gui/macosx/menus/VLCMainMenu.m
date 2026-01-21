199929ce3fb4f643f538bab334b88325b33e27da
Dave Nicolson <david.nicolson@gmail.com>
2026-01-21 18:52:39 +0000

    macosx: Ensure the audio player is visible

--- modules/gui/macosx/menus/VLCMainMenu.m
+++ modules/gui/macosx/menus/VLCMainMenu.m
@@ -1512,6 +1512,7 @@ typedef NS_ENUM(NSInteger, VLCObjectType) {
 
 - (IBAction)showDetachedAudioWindow:(id)sender
 {
+    [NSApp activateIgnoringOtherApps:YES];
     [VLCMain.sharedInstance.detachedAudioWindow makeKeyAndOrderFront:self];
 }
 
