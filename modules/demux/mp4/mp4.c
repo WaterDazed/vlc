0bc7900b8981a38f133a92e5aae3c9788da4f9a4
Steve Lhomme <robux4@ycbcr.xyz>
2026-01-30 08:26:58 +0100

    demux: mp4: rename track iterator to track_num
    No functional changes.

--- modules/demux/mp4/mp4.c
+++ modules/demux/mp4/mp4.c
@@ -4823,37 +4823,37 @@ static int ProbeFragments( demux_t *p_demux, bool b_force, bool *pb_fragmented )
                 if( p_moof->i_type != ATOM_moof )
                     continue;
 
-                for( unsigned i=0; i<p_sys->i_tracks; i++ )
+                for( unsigned track_num=0; track_num<p_sys->i_tracks; track_num++ )
                 {
                     MP4_Box_t *p_tfdt = NULL;
-                    MP4_Box_t *p_traf = MP4_GetTrafByTrackID( p_moof, p_sys->track[i].i_track_ID );
+                    MP4_Box_t *p_traf = MP4_GetTrafByTrackID( p_moof, p_sys->track[track_num].i_track_ID );
                     if( p_traf )
                         p_tfdt = MP4_BoxGet( p_traf, "tfdt" );
 
                     if( p_tfdt && BOXDATA(p_tfdt) )
                     {
-                        pi_track_times[i] = p_tfdt->data.p_tfdt->i_base_media_decode_time;
+                        pi_track_times[track_num] = p_tfdt->data.p_tfdt->i_base_media_decode_time;
                     }
                     else if( index == 0 ) /* Set first fragment time offset from moov */
                     {
-                        stime_t i_duration = GetMoovTrackDuration( p_sys, p_sys->track[i].i_track_ID );
-                        pi_track_times[i] = MP4_rescale( i_duration, p_sys->i_timescale, p_sys->track[i].i_timescale );
+                        stime_t i_duration = GetMoovTrackDuration( p_sys, p_sys->track[track_num].i_track_ID );
+                        pi_track_times[track_num] = MP4_rescale( i_duration, p_sys->i_timescale, p_sys->track[track_num].i_timescale );
                     }
 
-                    stime_t i_movietime = MP4_rescale( pi_track_times[i], p_sys->track[i].i_timescale, p_sys->i_timescale );
-                    p_sys->p_fragsindex->p_times[index * p_sys->i_tracks + i] = i_movietime;
+                    stime_t i_movietime = MP4_rescale( pi_track_times[track_num], p_sys->track[track_num].i_timescale, p_sys->i_timescale );
+                    p_sys->p_fragsindex->p_times[index * p_sys->i_tracks + track_num] = i_movietime;
 
                     stime_t i_duration = 0;
-                    if( GetMoofTrackDuration( p_sys->p_moov, p_moof, p_sys->track[i].i_track_ID, &i_duration ) )
-                        pi_track_times[i] += i_duration;
+                    if( GetMoofTrackDuration( p_sys->p_moov, p_moof, p_sys->track[track_num].i_track_ID, &i_duration ) )
+                        pi_track_times[track_num] += i_duration;
                 }
 
                 p_sys->p_fragsindex->pi_pos[index++] = p_moof->i_pos;
             }
 
-            for( unsigned i=0; i<p_sys->i_tracks; i++ )
+            for( unsigned track_num=0; track_num<p_sys->i_tracks; track_num++ )
             {
-                stime_t i_movietime = MP4_rescale( pi_track_times[i], p_sys->track[i].i_timescale, p_sys->i_timescale );
+                stime_t i_movietime = MP4_rescale( pi_track_times[track_num], p_sys->track[track_num].i_timescale, p_sys->i_timescale );
                 if( p_sys->p_fragsindex->i_last_time < i_movietime )
                     p_sys->p_fragsindex->i_last_time = i_movietime;
             }
