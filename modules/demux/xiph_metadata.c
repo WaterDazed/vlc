93a763e257ed7414e66071ab90ba970d0177ddc2
Francois Cartegnie <fcvlcdev@free.fr>
2026-02-10 18:56:56 +0700

    xiph: use reallocarray

--- modules/demux/xiph_metadata.c
+++ modules/demux/xiph_metadata.c
@@ -148,19 +148,12 @@ static seekpoint_t * getChapterEntry( unsigned int i_index, chapters_array_t *p_
         unsigned int i_newsize = p_array->i_size;
         while( i_index >= i_newsize ) i_newsize += 50;
 
-        if ( !p_array->pp_chapters )
-        {
-            p_array->pp_chapters = calloc( i_newsize, sizeof( seekpoint_t * ) );
-            if ( !p_array->pp_chapters ) return NULL;
-            p_array->i_size = i_newsize;
-        } else {
-            seekpoint_t **tmp = calloc( i_newsize, sizeof( seekpoint_t * ) );
-            if ( !tmp ) return NULL;
-            memcpy( tmp, p_array->pp_chapters, p_array->i_size * sizeof( seekpoint_t * ) );
-            free( p_array->pp_chapters );
-            p_array->pp_chapters = tmp;
-            p_array->i_size = i_newsize;
-        }
+        seekpoint_t **tmp = vlc_reallocarray( p_array->pp_chapters, i_newsize, sizeof( seekpoint_t * ) );
+        if( !tmp )
+            return NULL;
+        memset( &tmp[p_array->i_size], 0, (i_newsize - p_array->i_size) * sizeof( seekpoint_t * ) );
+        p_array->pp_chapters = tmp;
+        p_array->i_size = i_newsize;
     }
     if ( !p_array->pp_chapters[i_index] )
         p_array->pp_chapters[i_index] = vlc_seekpoint_New();
