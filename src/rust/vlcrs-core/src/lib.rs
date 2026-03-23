fb5f193c9a0fb63c0edbf63859fc82f6f165c939
Alaric Senat <alaric@videolabs.io>
2026-03-23 15:03:02 +0100

    vlcrs-core: remove license header typo

--- src/rust/vlcrs-core/src/lib.rs
+++ src/rust/vlcrs-core/src/lib.rs
@@ -34,7 +34,7 @@ compile_error!(
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, write to the Free Software Foundation,
-// Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.#![deny(unsafe_op_in_unsafe_fn)]
+// Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
 
 pub mod plugin;
 
