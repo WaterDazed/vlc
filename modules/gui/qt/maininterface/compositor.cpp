WaterDazed/vlc

commit 455c91cf004a2a6367e1a1b931654d4b264c07dd

qt: do not unnecessarily keep the component for `MainInterface.qml` around
We do not create `MainInterface.qml` multiple times, keeping the component
around is not necessary and consumes memory unnecessarily.

modules/gui/qt/maininterface/mainui.hpp
@@ -37,7 +37,7 @@ private:
     MainCtx* m_mainCtx = nullptr;
     QWindow*       m_interfaceWindow = nullptr;
 
-    QQmlComponent* m_component = nullptr;
+    QPointer<QQmlComponent> m_component;
     QQuickItem* m_rootItem = nullptr;
 
     QPointer<QQmlEngine> m_engineBound;

modules/gui/qt/maininterface/mainui.cpp
@@ -177,27 +177,34 @@ bool MainUI::setup(QQmlEngine* engine)
 
 QQuickItem* MainUI::createRootItem()
 {
+    if (m_rootItem)
+        return m_rootItem;
+
+    assert(m_component);
+
     QObject* rootObject = m_component->create();
 
     if (m_component->isError())
     {
         for(auto& error: m_component->errors())
             msg_Err(m_intf, "qml loading %s %s:%u", qtu(error.description()), qtu(error.url().toString()), error.line());
-        return nullptr;
+        goto end;
     }
 
     if (rootObject == nullptr)
     {
         msg_Err(m_intf, "unable to create main interface, no root item");
-        return nullptr;
+        goto end;
     }
     m_rootItem = qobject_cast<QQuickItem*>(rootObject);
     if (!m_rootItem)
     {
         msg_Err(m_intf, "unexpected type of qml root item");
-        return nullptr;
+        goto end;
     }
 
+end:
+    delete m_component; // No need to have the component around anymore.
     return m_rootItem;
 }
 

commit 12455e71c969a9b9fd7b8579cba8b041b0790755

qt: fix use after free in `CompositorVideo::commonGUICreate()`
We do not keep the `QQmlComponent` around for the root item
because the root item is not meant to be instantiated multiple
times, thanks to 455c91cf.

Calling `::createRootItem()` therefore deletes the component
after it creates the root item.

In this case, when `::setContent()` is used, it first gets
a valid `QQmlComponent` since `::getComponent()` is called
first, and then the component is deleted when `::createRootItem()`
is called. If the parameter order was different, such bug
would not exist (since calling `::getComponent()` after
calling `::createRootItem()` would return null pointer).

Unfortunately `QQuickView` takes the component as a raw pointer
and not a weak pointer, so there is not much we can do about
that here besides not providing the component. I still changed
the `::getComponent()` to return a weak pointer, though.

That being said, we should not really provide the component
anyway because we manage the creation of the root item. I
have checked the source code of `QQuickView`, and it does
not seem to actually require the component. Providing both
the component and the item does not make much sense, and I
assume that is why `QQuickView::setContent()` is marked as
internal.

Regression since 455c91cf.

modules/gui/qt/maininterface/compositor.cpp
@@ -381,7 +381,7 @@ bool CompositorVideo::commonGUICreate(QWindow* window, QmlUISurface* qmlSurface,
     ret = m_ui->setup(qmlSurface->engine());
     if (! ret)
         return false;
-    qmlSurface->setContent(m_ui->getComponent(), m_ui->createRootItem());
+    qmlSurface->setContent(nullptr, m_ui->createRootItem());
     return true;
 }
 
@@ -393,7 +393,7 @@ bool CompositorVideo::commonGUICreate(QWindow* window, QQuickView* qmlView, Comp
     ret = m_ui->setup(qmlView->engine());
     if (! ret)
         return false;
-    qmlView->setContent(QUrl(), m_ui->getComponent(), m_ui->createRootItem());
+    qmlView->setContent(QUrl(), nullptr, m_ui->createRootItem());
     return true;
 }
 

modules/gui/qt/maininterface/mainui.hpp
@@ -24,7 +24,7 @@ public:
 
     [[nodiscard]] bool setup(QQmlEngine* engine);
 
-    inline QQmlComponent* getComponent() const {return m_component;}
+    QPointer<QQmlComponent> getComponent() const {return m_component;}
     VLC_USED
     QQuickItem* createRootItem();
 